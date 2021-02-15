ddf5c2a7ee0ae97afe7c0d5a11811a33c739562a
Julian Scholte <julianscholte@gmail.com>
2021-02-15 11:23:24 +0100

    Update skybox.js

--- classes/skybox.js
+++ classes/skybox.js
@@ -1,13 +1,20 @@
 class Skybox {
-  constructor(width = 5000, height = 5000, depth = 5000, directions = null) {
+  constructor(
+    width = 5000,
+    height = 5000,
+    depth = 5000,
+    skyboxdirectory = "./assets/skybox/",
+    skyboximages = null
+  ) {
     this.width = width;
     this.height = height;
     this.depth = depth;
+    this.skyboxdirectory = skyboxdirectory;
 
-    if (directions) {
-      this.directions = directions;
+    if (skyboximages) {
+      this.skyboximages = skyboximages;
     } else {
-      this.directions = [
+      this.skyboximages = [
         "right.bmp",
         "left.bmp",
         "top.bmp",
@@ -20,11 +27,12 @@ class Skybox {
 
   addSkybox(scene) {
     let materialArray = [];
+
     for (let i = 0; i < 6; i++) {
       materialArray.push(
         new THREE.MeshBasicMaterial({
           map: new THREE.TextureLoader().load(
-            `./assets/skybox/${this.directions[i]}`
+            `${this.skyboxdirectory}${this.skyboximages[i]}`
           ),
           side: THREE.BackSide,
         })
@@ -36,6 +44,7 @@ class Skybox {
       this.height,
       this.depth
     );
+
     let skyBox = new THREE.Mesh(skyGeometry, materialArray);
     scene.add(skyBox);
   }
