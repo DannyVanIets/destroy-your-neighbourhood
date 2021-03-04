a7a4b1dd59b0c9de7eef4f79bcad0cdfa12df630
Julian Scholte <julianscholte@gmail.com>
2021-03-04 15:06:51 +0100

    Update MatrixTests.cs

--- Assignment 2/NUnitTestsMatrixTransformations/MatrixTests.cs
+++ Assignment 2/NUnitTestsMatrixTransformations/MatrixTests.cs
@@ -43,9 +43,9 @@ namespace NUnitTestsMatrixTransformations
             Assert.AreEqual(z, matrix.mat[2, 0]);
         }
 
-        [TestCase(0, 0, 0, 0,          0, 0, 0, 0,         0, 0, 0, 0)]
-        [TestCase(5, 4, 3, 2,          5, 4, 3, 2,         10, 8, 6, 4)]
-        [TestCase(-5, 32, 88, 99,      -15, -16, 34, 46,   -20, 16, 122, 145)]
+        [TestCase(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)]
+        [TestCase(5, 4, 3, 2, 5, 4, 3, 2, 10, 8, 6, 4)]
+        [TestCase(-5, 32, 88, 99, -15, -16, 34, 46, -20, 16, 122, 145)]
         public void Test_Matrix_Plus_Matrix(int m11, int m12, int m21, int m22, int secondm11, int secondm12, int secondm21, int secondm22, int expectedm11, int expectedm12, int expectedm21, int expectedm22)
         {
             // Arrange
@@ -56,15 +56,15 @@ namespace NUnitTestsMatrixTransformations
             Matrix result = matrix + matrix2;
 
             // Assert
-            Assert.AreEqual(expectedm11, result.mat[0,0]);
-            Assert.AreEqual(expectedm12, result.mat[0,1]);
-            Assert.AreEqual(expectedm21, result.mat[1,0]);
-            Assert.AreEqual(expectedm22, result.mat[1,1]);
+            Assert.AreEqual(expectedm11, result.mat[0, 0]);
+            Assert.AreEqual(expectedm12, result.mat[0, 1]);
+            Assert.AreEqual(expectedm21, result.mat[1, 0]);
+            Assert.AreEqual(expectedm22, result.mat[1, 1]);
         }
 
-        [TestCase(0, 0, 0, 0,         0, 0, 0, 0,         0, 0, 0, 0)]
-        [TestCase(5, 4, 3, 2,         10, 8, 6, 4,        -5, -4, -3, -2)]
-        [TestCase(-5, 32, 88, 99,     -15, -16, 34, 46,   10, 48, 54, 53)]
+        [TestCase(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)]
+        [TestCase(5, 4, 3, 2, 10, 8, 6, 4, -5, -4, -3, -2)]
+        [TestCase(-5, 32, 88, 99, -15, -16, 34, 46, 10, 48, 54, 53)]
         public void Test_Matrix_Minus_Matrix(int m11, int m12, int m21, int m22, int secondm11, int secondm12, int secondm21, int secondm22, int expectedm11, int expectedm12, int expectedm21, int expectedm22)
         {
             // Arrange
@@ -84,7 +84,7 @@ namespace NUnitTestsMatrixTransformations
         [TestCase(1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2)]
         [TestCase(10, 25, 59, 12, 12, 49, 29, 16, 845, 890, 1056, 3083)]
         [TestCase(-12, -45, -67, -12, 5, -1, -65, 18, 2865, -798, 445, -149)]
-        public void Test_Matrix_Multiplication(int m11, int m12, int m21, int m22, int secondm11, int secondm12, int secondm21, int secondm22, int expectedm11, int expectedm12, int expectedm21, int expectedm22)
+        public void Test_Matrix_Multiplication_Matrix(int m11, int m12, int m21, int m22, int secondm11, int secondm12, int secondm21, int secondm22, int expectedm11, int expectedm12, int expectedm21, int expectedm22)
         {
             // Arrange
             Matrix m1 = new Matrix(m11, m12, m21, m22);
